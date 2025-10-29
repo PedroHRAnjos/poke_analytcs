c0c9c6efb43c0c385ccb112dac03a441f4377386
PedroHRAnjos <pedroh.rodriguesanjos@gmail.com>
2025-10-29 12:37:24 -0300

    tabela com informações básicas dos pokémons

---
+++ tb_pokedex.py
@@ -0,0 +1,26 @@
+import json
+import requests
+
+url = "https://pokeapi.co/api/v2/pokemon/"
+pokemon_list = list()
+
+while url != None:
+    payload = {}
+    headers = {}
+    response = json.loads(requests.request("GET", url, headers=headers, data=payload).text)
+    url = response["next"]
+
+    for item in response["results"]:
+        pokemon_name = item["name"]
+        url_pokemon = f"https://pokeapi.co/api/v2/pokemon/{pokemon_name}"
+        response_pokemon = json.loads(requests.request("GET", url_pokemon, headers=headers, data=payload).text)
+
+        infos = {
+            "name": pokemon_name,
+            "id": response_pokemon["id"],
+            "height": response_pokemon["height"],
+            "weight": response_pokemon["weight"],
+            "is_default": response_pokemon["is_default"]
+        }
+        pokemon_list.append(infos)
+        print(infos)
